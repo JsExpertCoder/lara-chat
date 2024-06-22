8604e6b79c3676ccfcbf2cfb539cbb6942513fef
JsExpertCoder <misterjs247000@gmail.com>
2024-06-22 14:37:16 +0100

    channels: chat room added

--- routes/channels.php
+++ routes/channels.php
@@ -7,3 +7,7 @@ use App\Models\User;
 Broadcast::channel('chat.{receiverId}', function (User $user, int $receiverId) {
     return (int) $user->id === (int) $receiverId;
 });
+
+Broadcast::channel('chat.room.{roomId}', function ($user, $roomId) {
+    return $user instanceof User;
+});
